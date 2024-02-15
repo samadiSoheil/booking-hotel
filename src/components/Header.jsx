cb075b9b39f2c7298f9a3f5fd531d8df41c8f821
itsSoheil <soheil.samadi79@gmail.com>
2024-02-15 11:55:41 +0330

    Complete Header overview

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,43 @@
+import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiSearch } from "react-icons/hi";
+
 export default function Header() {
-  return <h1>this is header!</h1>;
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        {/* ----- SEARCH PLACES ----- */}
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            type="text"
+            placeholder="where to go?"
+            className="headerSearchInput"
+            name="destination"
+            id="destination"
+          />
+          <span className="seperator"></span>
+        </div>
+
+        {/* ----- DATE PEAKER ----- */}
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2023/06/34</div>
+          <span className="seperator"></span>
+        </div>
+
+        {/* ----- DATE PEAKER ----- */}
+        <div className="headerSearchItem">
+          <div id="optionDropDown">1 adult &bull; 2 children &bull; 1 room</div>
+          <span className="seperator"></span>
+        </div>
+
+        {/* ----- SEARCH BUTTON ----- */}
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
