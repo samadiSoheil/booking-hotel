270167a99419d35eb3b1ab0952ea118cfa6ae471
itsSoheil <soheil.samadi79@gmail.com>
2024-02-15 11:30:48 +0330

    Crreate and add Header component document

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>starter file</h1>
+  return <Header />;
 }
 
 export default App;
-
---
+++ src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h1>this is header!</h1>;
+}
