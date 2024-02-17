9d1fbbb594f2e2adf4f62bee34dd197365673546
itsSoheil <soheil.samadi79@gmail.com>
2024-02-17 09:50:53 +0330

    Create GuestOptionList component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import { MdLocationOn } from "react-icons/md";
-import { HiCalendar, HiSearch } from "react-icons/hi";
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
+import { useRef, useState } from "react";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 export default function Header() {
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+
+  const handleOption = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "inc" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+
   return (
     <div className="header">
       <div className="headerSearch">
@@ -25,9 +43,19 @@ export default function Header() {
           <span className="seperator"></span>
         </div>
 
-        {/* ----- DATE PEAKER ----- */}
+        {/* ----- DROP DOWN ----- */}
         <div className="headerSearchItem">
-          <div id="optionDropDown">1 adult &bull; 2 children &bull; 1 room</div>
+          <div id="optionDropDown" onClick={() => setOpenOptions((is) => !is)}>
+            {options.adult} adult &bull; {options.children} children &bull; {options.room}{" "}
+            room
+          </div>
+          {openOptions && (
+            <GuestOptionList
+              options={options}
+              handleOption={handleOption}
+              setOpenOptions={setOpenOptions}
+            />
+          )}
           <span className="seperator"></span>
         </div>
 
@@ -37,6 +65,54 @@ export default function Header() {
             <HiSearch className="headerIcon" />
           </button>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function GuestOptionList({ options, handleOption, setOpenOptions }) {
+  const optionsRef = useRef();
+  useOutsideClick(optionsRef, "optionDropDown", () => setOpenOptions(false));
+  return (
+    <div className="guestOptions" ref={optionsRef}>
+      <OptionItem
+        handleOption={handleOption}
+        options={options}
+        type="adult"
+        minLimit={1}
+      />
+      <OptionItem
+        handleOption={handleOption}
+        options={options}
+        type="children"
+        minLimit={0}
+      />
+      <OptionItem
+        handleOption={handleOption}
+        options={options}
+        type="room"
+        minLimit={1}
+      />
+    </div>
+  );
+}
+
+function OptionItem({ options, type, minLimit, handleOption }) {
+  return (
+    <div className="guestOptionItem">
+      <span className="optionText">{type}</span>
+      <div className="optionCounter">
+        <button
+          className="optionCounterBtn"
+          disabled={options[type] <= minLimit}
+          onClick={() => handleOption(type, "dec")}
+        >
+          <HiMinus className="icon" />
+        </button>
+        <span className="optionCounterNumber">{options[type]}</span>
+        <button className="optionCounterBtn" onClick={() => handleOption(type, "inc")}>
+          <HiPlus className="icon" />
+        </button>
       </div>
     </div>
   );
