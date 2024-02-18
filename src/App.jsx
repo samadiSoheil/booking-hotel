9182d1d529f774efc2898d931c8cba5a4ef74661
itsSoheil <soheil.samadi79@gmail.com>
2024-02-18 11:52:43 +0330

    Add react-router-dom and create main Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header";
+import LocationList from "./components/locationList/LocationList";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Toaster />
       <Header />
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+      </Routes>
+      {/* <LocationList /> */}
     </>
   );
 }
