84dee43d2c75b2e7f8ff5119912c08e8bd1bdd98
itsSoheil <soheil.samadi79@gmail.com>
2024-02-18 09:34:26 +0330

    Add Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Toaster />
+      <Header />
+    </>
+  );
 }
 
 export default App;
